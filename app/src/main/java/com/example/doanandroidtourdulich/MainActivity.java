f6b13a9a754a5acf94d8a2ab585394ee10d380ad
duchu <duchuygiang224@mail.com>
2023-03-29 19:33:57 +0700

    add Menu

--- app/src/main/java/com/example/doanandroidtourdulich/MainActivity.java
+++ app/src/main/java/com/example/doanandroidtourdulich/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.doanandroidtourdulich;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
---
+++ app/src/main/java/com/example/doanandroidtourdulich/itemMenu.java
@@ -0,0 +1,11 @@
+package com.example.doanandroidtourdulich;
+
+public class itemMenu {
+    public String tenItem;
+    public int icon;
+
+    public itemMenu(String tenItem, int icon) {
+        this.tenItem = tenItem;
+        this.icon = icon;
+    }
+}
