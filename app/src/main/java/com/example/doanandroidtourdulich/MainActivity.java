afbc378b3bd097cc78f9bf5d1255885babf8ddc3
duchu <duchuygiang224@mail.com>
2023-03-30 08:09:48 +0700

    add Menu

--- app/src/main/java/com/example/doanandroidtourdulich/MainActivity.java
+++ app/src/main/java/com/example/doanandroidtourdulich/MainActivity.java
@@ -26,43 +26,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        anhXa();
-        actionToolBar();
-        actionMenu();
+
+
 
     }
 
-    private void actionMenu() {
-        arrayList = new ArrayList<>();
-        arrayList.add(new itemMenu("support",R.drawable.iconSupport));
-        arrayList.add(new itemMenu("shop",R.drawable.iconShop));
-        arrayList.add(new itemMenu("ticket",R.drawable.iconTicket));
-        adapter = new menuAdapter(this, R.layout.dong_item, arrayList);
-        listView.setAdapter(adapter);
-
-    }
-
-    private void actionToolBar() {
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationIcon(R.drawable.ic_action_menu);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-    }
-
-    private void setSupportActionBar(Toolbar toolbar) {
-    }
 
 
-    private void anhXa() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        navigationView = (NavigationView) findViewById(R.id.navigationView);
-        listView = (ListView) findViewById(R.id.lv);
-    }
+
 
 }
