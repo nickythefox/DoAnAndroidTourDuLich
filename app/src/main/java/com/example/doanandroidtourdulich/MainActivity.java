3e27d1081dfb873b1166e71c0d692845215d9a59
duchu <duchuygiang224@mail.com>
2023-04-02 21:55:13 +0700

    Fix Menu

--- app/src/main/java/com/example/doanandroidtourdulich/MainActivity.java
+++ app/src/main/java/com/example/doanandroidtourdulich/MainActivity.java
@@ -1,26 +1,50 @@
 package com.example.doanandroidtourdulich;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.Toolbar;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.example.doanandroidtourdulich.fragment.HomeFragment;
+import com.example.doanandroidtourdulich.ui.Login;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    ListView listView;
-    ArrayList<itemMenu> arrayList;
-    menuAdapter adapter;
+    ActionBarDrawerToggle drawerToggle;
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            int id = item.getItemId();
+            if (id == R.id.nav_view)
+            {
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +52,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home: {
+                        Toast.makeText(MainActivity.this, "Home Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.support: {
+                        Toast.makeText(MainActivity.this, "Support Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.ticket: {
+                        Toast.makeText(MainActivity.this, "Ticket Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.login: {
+                        Toast.makeText(MainActivity.this, "LogIn Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.logout: {
+                        Toast.makeText(MainActivity.this, "LogOut Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-
-
-
+    public void logout(View view) {
+        AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+        dlg.setTitle("Hỏi");
+        dlg.setMessage("Bạn có muốn đăng xuất?");
+        dlg.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        dlg.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        dlg.create().show();
+    }
 }
